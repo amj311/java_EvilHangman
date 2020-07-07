b483ac8db35b61c4e1ac7d824f8e98c51b7bf3dc
amj311 <amjudd315@gmail.com>
2020-07-06 20:26:47 -0600

    many changes. passed tests

--- src/hangman/EvilHangman.java
+++ src/hangman/EvilHangman.java
@@ -1,9 +1,84 @@
 package hangman;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class EvilHangman {
+    private static EvilHangmanGame game = new EvilHangmanGame();
+    static int numGuesses = 0;
+    static int prevNumLetters;
 
     public static void main(String[] args) {
+        String dictionary = args[0];
+        int wordLength = Integer.parseInt(args[1]);
+        numGuesses = Integer.parseInt(args[2]);
 
+        File dictFile = new File(dictionary);
+
+        try {
+            game.startGame(dictFile, wordLength);
+        }
+        catch(IOException e) {
+            System.out.println(e.toString());
+        }
+        catch(EmptyDictionaryException e) {
+            System.out.println("⚠ "+e.toString()+": No words of length "+wordLength+" found in "+dictionary);
+        }
+
+        System.out.println("Word: "+game.getWordDisplay());
+        runRound();
+        return;
     }
 
+    public static void runRound() {
+        System.out.println("Guess a letter! ("+numGuesses+" guesses left)");
+
+        try {
+            Scanner userIn = new Scanner(System.in);
+            String guessStr = userIn.nextLine().toLowerCase();
+            Set<String> possibilities = new HashSet<>();
+
+            // Make sure input char is between 'a' and 'z'
+            if (guessStr.length() == 0 || guessStr.charAt(0) <= 96 || 123 <= guessStr.charAt(0)) {
+                System.out.println("⚠ Invalid Input!");
+                runRound();
+            }
+
+            try {
+                possibilities = game.makeGuess(guessStr.charAt(0));
+            } catch (GuessAlreadyMadeException e) {
+                System.out.println("⚠ You already guessed that!");
+                runRound();
+            };
+
+            if (prevNumLetters <= game.getNumLettersLeft()) numGuesses--;
+            prevNumLetters = game.getNumLettersLeft();
+
+            System.out.println("Word: "+game.getWordDisplay());
+
+            if (game.getNumLettersLeft() > 0) {
+                if (numGuesses > 0) {
+                    System.out.println("Guessed letters: "+game.getGuessedLetters().toString());
+                    runRound();
+                }
+                else {
+                    System.out.println("No more guesses! Sorry.");
+                    System.out.println("The word was: "+possibilities.toArray()[0]);
+                    return;
+                }
+            }
+            else  {
+                System.out.println("Congrats!");
+                System.out.println("The word was: "+possibilities.toArray()[0]);
+                return;
+            }
+
+        } catch (StringIndexOutOfBoundsException e) {
+//            System.out.println("⚠ Invalid Input!");
+            runRound();
+        }
+    }
 }
--- src/hangman/EvilHangmanGame.java
+++ src/hangman/EvilHangmanGame.java
@@ -5,28 +5,155 @@ import java.io.IOException;
 import java.util.*;
 
 public class EvilHangmanGame implements IEvilHangmanGame {
-    Set<String> words = new HashSet<String>();
+    Set<String> words = new HashSet<>();
+    SortedSet<Character> guessedLetters = new TreeSet<>();
+    String wordDisplay = "";
+    int numLettersLeft;
 
     @Override
     public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
+        words = new HashSet<>();
+        guessedLetters = new TreeSet<>();
+        wordDisplay = "";
+
         // find dictionary words of length WordLength
         Scanner myReader = new Scanner(dictionary);
         while (myReader.hasNext()) {
             String data = myReader.next();
-            if (data.length() == wordLength) words.add(data);
+            if (data.length() == wordLength) {
+                words.add(data);
+//                System.out.println(data);
+            }
         }
         myReader.close();
 
         if (words.size() == 0) throw new EmptyDictionaryException();
+
+        StringBuilder newWordDisplay = new StringBuilder();
+        while (newWordDisplay.length() < wordLength) newWordDisplay.append("-");
+        wordDisplay = newWordDisplay.toString();
+        numLettersLeft = wordLength;
     }
 
     @Override
     public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
-        return null;
+        guess = (""+guess).toLowerCase().charAt(0);
+
+        if (guessedLetters.contains(guess)) throw new GuessAlreadyMadeException();
+        else guessedLetters.add(guess);
+
+        // create map of all words with matching posVectors, keeping track of the largest one
+        Set<Vector<Integer>> allCodes = new HashSet<>();
+        Map<Vector<Integer>, HashSet<String>> codeToWordsMap = new HashMap<>();
+        for (String word : words) {
+            Vector<Integer> wordPosCode = getPosCode(word, guess);
+            allCodes.add(wordPosCode);
+//            System.out.println(word+": "+wordPosCode);
+            if (codeToWordsMap.containsKey(wordPosCode)) {
+                Set<String> setForCode = codeToWordsMap.get(wordPosCode);
+                setForCode.add(word);
+            }
+            else {
+                HashSet<String> newSet = new HashSet<>();
+                newSet.add(word);
+                codeToWordsMap.put(getPosCode(word, guess), newSet);
+            }
+        }
+
+        // select set to return
+        Set<Vector<Integer>> eliminationSet = new HashSet<>(allCodes);
+        for (Vector<Integer> code : allCodes) {
+
+//            System.out.println(eliminationSet.toString());
+//            System.out.println(code);
+            Set<String> codeSet = codeToWordsMap.get(code);
+            for (Vector<Integer> otherCode : allCodes) {
+//                System.out.println("    "+otherCode);
+
+                if (otherCode == code) continue;
+                if (!eliminationSet.contains(code)) continue;
+                if (!eliminationSet.contains(otherCode)) continue;
+                if (eliminationSet.size() == 1) break;
+
+                Set<String> otherCodeSet = codeToWordsMap.get(otherCode);
+                // Eliminate otherCode if its set is smaller than the first
+                if (otherCodeSet.size() > codeSet.size()) {
+                    eliminationSet.remove(code);
+                    continue;
+                } else if (codeSet.size() > otherCodeSet.size()) {
+                    eliminationSet.remove(otherCode);
+                    continue;
+                } else if (code.size() != otherCode.size()) {
+                    if (code.size() == 0) {
+                        eliminationSet.remove(otherCode);
+                        continue;
+                    } else if (otherCode.size() == 0) {
+                        eliminationSet.remove(code);
+                        continue;
+                    } else if (code.size() < otherCode.size()) {
+                        eliminationSet.remove(otherCode);
+                        continue;
+                    } else if (otherCode.size() < code.size()) {
+                        eliminationSet.remove(code);
+                        continue;
+                    }
+                } else {
+                    for (int i = code.size() - 1; i >= 0; i--) {
+                        if (code.elementAt(i) > otherCode.elementAt(i)) {
+                            eliminationSet.remove(otherCode);
+                            continue;
+                        } else if (otherCode.elementAt(i) > code.elementAt(i)) {
+                            eliminationSet.remove(code);
+                            continue;
+                        }
+                    }
+                }
+
+
+            }
+        }
+
+
+        //update words set
+        words = codeToWordsMap.get(eliminationSet.toArray()[0]);
+
+        // update wordDisplay.
+        addGuessToDisplay(guess, (Vector<Integer>) eliminationSet.toArray()[0]);
+//        System.out.println(wordDisplay);
+        return words;
+    }
+
+    public Vector<Integer> getPosCode(String word, char guess) {
+        Vector<Integer> posVector = new Vector<>();
+        for (int c = 0; c < word.length(); c++) {
+            if (word.charAt(c) == guess) posVector.add(c);
+        }
+        return posVector;
     }
 
     @Override
     public SortedSet<Character> getGuessedLetters() {
-        return null;
+        return guessedLetters;
+    }
+
+    public String getWordDisplay() {
+        return wordDisplay;
+    }
+
+    private void addGuessToDisplay(char guess, Vector<Integer> posVector) {
+//        System.out.println(guess);
+//        System.out.println(posVector);
+        numLettersLeft -= posVector.size();
+
+        StringBuilder newDisplay = new StringBuilder(wordDisplay);
+        for (int p = 0; p < posVector.size(); p++) {
+            int pos = Integer.parseInt(String.valueOf(posVector.elementAt(p)));
+            newDisplay.replace(pos, pos+1, String.valueOf(guess));
+        }
+        wordDisplay = newDisplay.toString();
+    }
+
+    public int getNumLettersLeft() {
+        return numLettersLeft;
     }
 }
